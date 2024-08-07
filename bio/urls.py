bf3111033f28bc0523fd211deb5d26a1ed9e26b3
Daniru <daniil.nikiforenko03@mail.ru>
2024-08-07 14:09:44 +0700

    ref/CBV для bio

--- bio/urls.py
+++ bio/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from . import views
-from main.views import WritersDetailView
+from .views import BioHomeView, BioPushkinView, BioAkhmatovaView, \
+                    BioDostoevskyView, BioEseninView, BioGorkyView, \
+                    BioLermontovView, BioMayakovskyView, BioTsvetaevaView
 
 urlpatterns = [
-    path('', views.bio_home, name='bio_home'),
-    path('pushkin', views.bio_pushkin, name='bio_pushkin'),
-    path('akhmatova', views.bio_akhmatova, name='bio_akhmatova'),
-    path('dostoevsky', views.bio_dostoevsky, name='bio_dostoevsky'),
-    path('esenin', views.bio_esenin, name='bio_esenin'),
-    path('gorky', views.bio_gorky, name='bio_gorky'),
-    path('lermontov', views.bio_lermontov, name='bio_lermontov'),
-    path('mayakovsky', views.bio_mayakovsky, name='bio_mayakovsky'),
-    path('tsvetaeva', views.bio_tsvetaeva, name='bio_tsvetaeva')
+    path('', BioHomeView.as_view(), name='bio_home'),
+    path('pushkin/', BioPushkinView.as_view(), name='bio_pushkin'),
+    path('akhmatova/', BioAkhmatovaView.as_view(), name='bio_akhmatova'),
+    path('dostoevsky', BioDostoevskyView.as_view(), name='bio_dostoevsky'),
+    path('esenin', BioEseninView.as_view() , name='bio_esenin'),
+    path('gorky', BioGorkyView.as_view(), name='bio_gorky'),
+    path('lermontov', BioLermontovView.as_view(), name='bio_lermontov'),
+    path('mayakovsky', BioMayakovskyView.as_view(), name='bio_mayakovsky'),
+    path('tsvetaeva', BioTsvetaevaView.as_view(), name='bio_tsvetaeva')
 ] 
--- bio/views.py
+++ bio/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render
+from django.views import View
 from main.models import Writers
 
-def bio_home(request):
-    persons = Writers.objects.order_by('name')
-    return render(request, 'bio/bio_home.html', {'persons':persons})
+class BioHomeView(View):
+    def get(self, request):
+        persons = Writers.objects.order_by('name')
+        return render(request, 'bio/bio_home.html', {'persons': persons})
 
-def bio_pushkin(request):
-    return render(request, 'bio/bio_pushkin.html')
+class BioDetailView(View):
+    template_name = None
 
-def bio_akhmatova(request):
-    return render(request, 'bio/bio_akhmatova.html')
+    def get(self, request):
+        return render(request, self.template_name)
 
-def bio_dostoevsky(request):
-    return render(request, 'bio/bio_dostoevsky.html')
+class BioPushkinView(BioDetailView):
+    template_name = 'bio/bio_pushkin.html'
 
-def bio_esenin(request):
-    return render(request, 'bio/bio_esenin.html')
+class BioAkhmatovaView(BioDetailView):
+    template_name = 'bio/bio_akhmatova.html'
 
-def bio_gorky(request):
-    return render(request, 'bio/bio_gorky.html')
+class BioDostoevskyView(BioDetailView):
+    template_name = 'bio/bio_dostoevsky.html'
 
-def bio_lermontov(request):
-    return render(request, 'bio/bio_lermontov.html')
+class BioEseninView(BioDetailView):
+    template_name = 'bio/bio_esenin.html'
 
-def bio_mayakovsky(request):
-    return render(request, 'bio/bio_mayakovsky.html')
+class BioGorkyView(BioDetailView):
+    template_name = 'bio/bio_gorky.html'
 
-def bio_tsvetaeva(request):
-    return render(request, 'bio/bio_tsvetaeva.html')
+class BioLermontovView(BioDetailView):
+    template_name = 'bio/bio_lermontov.html'
+
+class BioMayakovskyView(BioDetailView):
+    template_name = 'bio/bio_mayakovsky.html'
+
+class BioTsvetaevaView(BioDetailView):
+    template_name = 'bio/bio_tsvetaeva.html'
